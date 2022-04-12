d4d2c1ebce051923000ab2fad3c9d3762566dc17
Mathys Ballagny <contact@mathysballagny.fr>
2022-04-13 01:44:58 +0200

    it compiles tickflow woo!

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const os = require('os');
+var exec = require('child_process').exec;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +20,57 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tickflow-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('tickflow.citraBuild', function () {
+		let config = vscode.workspace.getConfiguration('tickflow');
+		let tickompiler = config.get("tickompilerPath")
+		let base = config.get("basebinPath")
+		let javaVer = config.get("java16")
+		let current = vscode.workspace.workspaceFolders[0].uri.path
+		let file = vscode.window.activeTextEditor.document.fileName
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
+		let delimiter = ""
+		let citra = config.get("citraFolder")	//Special case for Citra, which uses a Windows var by default
+		if(os.platform() == "win32"){
+			citra = citra.replace('%appdata%', os.homedir+"\\appdata\\roaming")
+			current = current.split('/').join('\\').replace('\\','')//I hate filepaths
+			delimiter = "\\"
+		}
+		else if(os.platform() == "linux"){
+			citra = citra.replace('%appdata%', os.homedir)
+			delimiter = "/"
+		}
+		
+
+
+		fs.access(tickompiler, fs.constants.F_OK, (err)=>{
+			if(err != null)
+				vscode.window.showInformationMessage("Tickompiler not found!")
+		})
+		fs.access(base, fs.constants.F_OK, (err)=>{
+			if(err != null)
+				vscode.window.showInformationMessage("base.bin not found!")
+		})
+		fs.access(citra, fs.constants.F_OK, (err)=>{
+			if(err != null)
+				vscode.window.showInformationMessage("Citra's folder does not exist!")
+		})
+		// //We can execute the code now
+		let java =""
+		if(javaVer)
+			java = "java --add-opens java.base/java.lang=ALL-UNNAMED -jar "
+		else
+			java = "java -jar "
+		let command = java+tickompiler+' c "'+file+'" "'+current+delimiter+'bin'+delimiter+'"';
+		exec(command,
+		function (error, stdout, stderr) {
+			vscode.window.showInformationMessage(command);
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log(error);
+			}
+		});
+		
 	});
 
 	context.subscriptions.push(disposable);
